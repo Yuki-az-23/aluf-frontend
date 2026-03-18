1517d29294b4e6318bd450ad5153ecb71ad50b60
lex zgeria <az@zagent.live>
2026-03-18 12:46:41 +0200

    chore: set NODE_ENV to production in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   resolve: {
     alias: { '@': resolve(__dirname, 'src') },
   },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.tsx'),
